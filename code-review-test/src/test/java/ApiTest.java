d05d5950a8cf60b772485e374b74701b18020cee
Doar <2508356590@qq.com>
2025-04-14 15:22:14 +0800

    feat: init project

---
+++ code-review-test/src/test/java/ApiTest.java
@@ -0,0 +1,21 @@
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Author: Doar
+ * @CreateTime: 2025-04-14
+ * @Description:
+ */
+@Slf4j
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ApiTest {
+
+    @Test
+    public void test(){
+        System.out.println("cs");
+    }
+}
